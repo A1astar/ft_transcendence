8378cff6a8e0241dea05f9cc6bf544b1219ef010
Kai-lin Yang <kyang@f6r10s11.clusters.42paris.fr>
2025-09-30 18:21:09 +0200

    start matchmaking

--- app/services/game_orchestration/src/index.ts
+++ app/services/game_orchestration/src/index.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { randomUUID } from "crypto";
+import { Player, Match, queues } from "./objects.js";
 
 const fastify = Fastify({ logger: true });
 
@@ -8,10 +10,70 @@ fastify.register(cors, {
   origin: "*"
 });
 
-// Minimal route
-fastify.get("/health", async (request, reply) => {
-  return { status: "ok", service: "user-service" };
-});
+fastify.post("/matchmaking/local", async(request, reply) => {
+  const player = request.body as Player;
+  queues.local.push(player);
+
+  if (queues.local.length == 2) {
+    const matchPlayers = queues.local.splice(0,2);
+    const match: Match = {
+      id: randomUUID(),
+      players: matchPlayers,
+      mode: "local",
+      tournament_id: 0,
+      status: "running"
+    };
+    return match;
+  }
+  return {status: "waiting"};
+})
+
+fastify.post("/matchmaking/remote", async(request, reply) => {
+  const player = request.body as Player;
+  queues.remote.push(player);
+
+  if (queues.remote.length == 2) {
+    const matchPlayers = queues.remote.splice(0,2);
+    const match: Match = {
+      id: randomUUID(),
+      players: matchPlayers,
+      mode: "remote",
+      tournament_id: 0,
+      status: "running"
+    };
+    return match;
+  }
+  return {status: "waiting"};
+})
+
+// todo
+fastify.post("/matchmaking/tournament", async(request, reply) => {
+  const player = request.body as Player;
+  queues.tournament.push(player);
+
+  if (queues.tournament.length == 4) {
+
+    const matchPlayers1 = queues.tournament.splice(0,2);
+    const match1: Match = {
+      id: randomUUID(),
+      players: matchPlayers1,
+      mode: "tournament",
+      tournament_id: 1,
+      status: "running"
+    };
+    const matchPlayers2 = queues.tournament.splice(0,2);
+    const match2: Match = {
+      id: randomUUID(),
+      players: matchPlayers2,
+      mode: "tournament",
+      tournament_id: 2,
+      status: "running"
+    };
+    return match1;
+  }
+  return {status: "waiting"};
+})
+
 
 // Example user endpoint (dummy)
 fastify.get("/users/:id", async (request, reply) => {
