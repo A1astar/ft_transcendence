b665af30059314b4d324c17d83ab71deb23e271e
Kai-lin Yang <kyang@f6r7s1.clusters.42paris.fr>
2025-10-01 16:44:14 +0200

    sending game session to engine

---
+++ app/services/game-engine/src/index.ts
@@ -0,0 +1,29 @@
+import Fastify, { FastifyInstance } from "fastify";
+import cors from "@fastify/cors";
+
+
+// Start server
+const start = async () => {
+  
+  const fastify = Fastify({ logger: true });
+
+  // Enable CORS (allow connections from frontend or other services)
+  fastify.register(cors, {
+    origin: "*"
+  });
+
+  fastify.post("/game-engine/start", async(request, reply) => {
+    console.log(request.body);
+	  return request.body;
+	});
+
+  try {
+    await fastify.listen({ port: 3002, host: "0.0.0.0" });
+    console.log("Game Engine Service running on port 3002");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
