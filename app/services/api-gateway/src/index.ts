f12dc8990ee7fb6623c203c57ea8dc75df6d8086
Anska <103422661+algadea@users.noreply.github.com>
2025-10-08 16:44:12 +0200

    add frontend

--- app/services/api-gateway/src/index.ts
+++ app/services/api-gateway/src/index.ts
@@ -1,5 +1,34 @@
-import Fastify, { FastifyInstance } from 'fastify'
-import chalk from 'chalk'
+import Fastify, { FastifyInstance } from 'fastify';
+import fastifyStatic from '@fastify/static';
+import chalk from 'chalk';
+
+let page = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Transcendence</title>
+    </head>
+
+    <body>
+        <h1 align="center">Transcendence</h1>
+        <script>
+
+        </script>
+    </body>
+</html>
+`;
+
+function manageAuthentication() {
+    console.log(chalk.blue.bold("/authentication"));
+}
+
+function manageGameOrchestration() {
+    console.log(chalk.yellow.bold("/game-orchestration"));
+}
+
+function manageGameEngine() {
+    console.log(chalk.green.bold("/game-engine"));
+}
 
 async function routeRequest(fastify: FastifyInstance) {
     fastify.all('*', async(request, reply) => {
@@ -10,20 +39,30 @@ async function routeRequest(fastify: FastifyInstance) {
                 console.log(chalk.red.bold("/"));
                 break;
             case "/authentication":
-                console.log(chalk.blue.bold("/authentication"));
+                manageAuthentication();
                 break;
             case "/game-orchestration":
-                console.log(chalk.yellow.bold("/game-orchestration"));
+                manageGameOrchestration();
                 break;
             case "/game-engine":
-                console.log(chalk.green.bold("/game-engine"));
+                manageGameEngine();
                 break;
         }
+    });
+
+
+    fastify.get('/', async (request, reply) => {
+        console.log(chalk.magenta.bold("request: ") + chalk.blue.bold(request.url));
+        reply
+        .code(200)
+        .type('text/html')
+        .send(page);
     });
 }
 
 async function initAPIGateway(fastify: FastifyInstance) {
     fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
+    console.log(chalk.white.bold("API Gateway state: ") + chalk.green.bold.italic("running"));
     if (err) {
         fastify.log.error(err)
         throw err
@@ -32,13 +71,12 @@ async function initAPIGateway(fastify: FastifyInstance) {
 }
 
 async function main() {
-    const fastify = Fastify({
-        logger: true
-    })
+    // const fastify = Fastify({ logger: true });
+    const fastify = Fastify();
 
     try {
         initAPIGateway(fastify);
-        routeRequest(fastify);
+        await routeRequest(fastify);
 
     } catch (err) {
         console.log(err);
