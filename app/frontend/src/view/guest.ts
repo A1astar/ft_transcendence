4fe5ca17b9f0d0f0d45bb6a2d41f7963715b51cd
Alexandre Lacroix <alexandre.lacroix01@outlook.fr>
2025-10-17 13:08:06 +0200

    SPA view registrations phase ok

--- app/frontend/src/view/guest.ts
+++ app/frontend/src/view/guest.ts
@@ -1,32 +1,32 @@
 import {
     clearDiv,
-	createGifBackgroundDiv,
+	createVideoBackgroundDiv,
     createHeadingText,
     createSubheadingText,
     createFormElement,
     createInputElement,
     createLogoElement,
 	createButtonForm,
+	createBoxDiv
 } from "./domElements.js";
 
-const backgroundDiv = document.getElementById("background");
 const appDiv = document.getElementById("app");
 
 export function renderGuestLogin() {
-	if (backgroundDiv) {
-		clearDiv(backgroundDiv);
-		backgroundDiv.appendChild(createGifBackgroundDiv("../../backgrounds/Mordor.gif"));
-	}
 	if (appDiv) {
 		clearDiv(appDiv);
 
 		const formDiv = createFormElement("guestForm");
-		formDiv.appendChild(createSubheadingText("Join the realm of shadows as guest"));
 		formDiv.appendChild(createInputElement("text", "username", "Pseudo"));
 		formDiv.appendChild(createButtonForm("Continue as Guest", "guest"));
 
+		const guestBoxDiv = createBoxDiv("guestBoxDiv");
+		guestBoxDiv.appendChild(createSubheadingText("Join the realm of shadows as guest"));
+		guestBoxDiv.appendChild(formDiv);
+
+		appDiv.appendChild(createVideoBackgroundDiv("../../backgrounds/Gandalf.mp4"));
 		appDiv.appendChild(createLogoElement("../icons/sauron.png", "Barad-dûr Logo"));
 		appDiv.appendChild(createHeadingText("Lord of Transcendence"));
-		appDiv.appendChild(formDiv);
+		appDiv.appendChild(guestBoxDiv);
 	}
 }
--- app/frontend/src/view/notFound.ts
+++ app/frontend/src/view/notFound.ts
@@ -4,21 +4,22 @@ import {
     createHeadingText,
     createParagraphText,
     createButtonLink,
+	createBoxDiv
 } from "./domElements.js";
 
-const backgroundDiv = document.getElementById("background");
 const appDiv = document.getElementById("app");
 
 export function renderNotFound() {
-	if (backgroundDiv) {
-		clearDiv(backgroundDiv);
-		backgroundDiv.appendChild(createGifBackgroundDiv("../../backgrounds/Mordor.gif"));
-	}
+
 	if (appDiv) {
 		clearDiv(appDiv);
 
+		const backToLoginBox = createBoxDiv("backToLoginBox");
+		backToLoginBox.appendChild(createParagraphText('"Not all those who wander are lost..."'));
+		backToLoginBox.appendChild(createButtonLink("/", "Back to the Mordor"));
+
+		appDiv.appendChild(createGifBackgroundDiv("../../backgrounds/Mordor.gif"));
 		appDiv.appendChild(createHeadingText("404: page not found"));
-		appDiv.appendChild(createParagraphText('"Not all those who wander are lost..."'));
-		appDiv.appendChild(createButtonLink("/", "Back to the Mordor"));
+		appDiv.appendChild(backToLoginBox);
 	}
 }
