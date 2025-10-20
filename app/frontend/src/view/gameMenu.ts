b43e6f3fa3c521728e65260e9ccf7f6fb9cac749
Alexandre Lacroix <alexandre.lacroix01@outlook.fr>
2025-10-20 11:31:56 +0200

    View fix ok

--- app/frontend/src/view/gameMenu.ts
+++ app/frontend/src/view/gameMenu.ts
@@ -1,44 +1,52 @@
 import {
     clearDiv,
-	createGifBackgroundDiv,
+	createVideoBackgroundDiv,
     createHeadingText,
     createSubheadingText,
     createFormElement,
     createButtonLink,
     createLogoElement,
 	createButtonForm,
+	createBoxDiv
+
 } from "./domElements.js";
 
-const backgroundDiv = document.getElementById("background");
 const appDiv = document.getElementById("app");
 
 export function renderGameMenu() {
-	if (backgroundDiv) {
-		clearDiv(backgroundDiv);
-		backgroundDiv.appendChild(createGifBackgroundDiv("../../backgrounds/Mordor.gif"));
-	}
 	if (appDiv) {
 		clearDiv(appDiv);
 
 		const LocalGameformDiv = createFormElement("localGameForm");
-		LocalGameformDiv.appendChild(createSubheadingText("Local Battle"));
 		LocalGameformDiv.appendChild(createButtonForm("Local", "local"));
 
 		const RemoteGameformDiv = createFormElement("remoteGameForm");
-		RemoteGameformDiv.appendChild(createSubheadingText("Remote Battle"));
 		RemoteGameformDiv.appendChild(createButtonForm("Remote 2 players", "Remote2"));
 		RemoteGameformDiv.appendChild(createButtonForm("Remote 4 players", "Remote4"));
 
 		const TournamentGameformDiv = createFormElement("tournamentGameForm");
-		TournamentGameformDiv.appendChild(createSubheadingText("Tournament for Middle-earth"));
 		TournamentGameformDiv.appendChild(createButtonForm("Tournament 4 players", "Tournament4"));
 		TournamentGameformDiv.appendChild(createButtonForm("Tournament 8 players", "Tournament8"));
 
+
+		const localBox = createBoxDiv("localBox");
+		localBox.appendChild(createSubheadingText("Local Battle"));
+		localBox.appendChild(LocalGameformDiv);
+
+		const remoteBox = createBoxDiv("remoteBox");
+		remoteBox.appendChild(createSubheadingText("Remote Battle"));
+		remoteBox.appendChild(RemoteGameformDiv);
+
+		const tournamentBox = createBoxDiv("tournamentBox");
+		tournamentBox.appendChild(createSubheadingText("Tournament for Middle-earth"));
+		tournamentBox.appendChild(TournamentGameformDiv);
+
+		appDiv.appendChild(createVideoBackgroundDiv("../backgrounds/Sauron.mp4"));
 		appDiv.appendChild(createLogoElement("../icons/sauron.png", "Barad-dûr Logo"));
 		appDiv.appendChild(createHeadingText("Lord of Transcendence"));
-		appDiv.appendChild(LocalGameformDiv);
-		appDiv.appendChild(RemoteGameformDiv);
-		appDiv.appendChild(TournamentGameformDiv);
+		appDiv.appendChild(localBox);
+		appDiv.appendChild(remoteBox);
+		appDiv.appendChild(tournamentBox);
 		appDiv.appendChild(createButtonLink("/profile", "Profile", "top-right"));
 		appDiv.appendChild(createButtonLink("/settings", "Settings", "top-left"));
 	}
