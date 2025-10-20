df9bc2773f3ae4e45eedf73d98b97a8a9b0b8f32
Alexandre Lacroix <alexandre.lacroix01@outlook.fr>
2025-10-20 11:51:18 +0200

    adding api calls in profileview.ts

--- app/frontend/src/view/profile.ts
+++ app/frontend/src/view/profile.ts
@@ -9,9 +9,11 @@ import {
 	createButtonForm,
 	createBoxDiv
 } from "./domElements.js";
+//import { ApiClient } from "../apiService.js";
 
 const appDiv = document.getElementById("app");
 
+//! Renders the profile page with placeholder data
 export function renderProfile() {
 	if (appDiv) {
 		clearDiv(appDiv);
@@ -49,3 +51,45 @@ export function renderProfile() {
 		appDiv.appendChild(sessionBoxDiv);
 	}
 }
+
+
+//! Renders the profile page with API call
+// export function renderProfile() {
+// 	if (appDiv) {
+// 		clearDiv(appDiv);
+
+// 		const userData = ApiClient.get("/authentication/userinfo")
+
+// 		const accountFormDiv = createFormElement("profileForm");
+// 		accountFormDiv.appendChild(createParagraphText("Username: " + userData.username));
+// 		accountFormDiv.appendChild(createParagraphText("Email: " + userData.email));
+
+// 		const sessionFormDiv = createFormElement("sessionForm");
+// 		sessionFormDiv.appendChild(createButtonForm("Logout", "logout"));
+
+// 		const statFormDiv = createFormElement("statForm");
+// 		statFormDiv.appendChild(createParagraphText("played: " + userData.gamePlayed));
+// 		statFormDiv.appendChild(createParagraphText("Won: " + userData.gameWon));
+// 		statFormDiv.appendChild(createParagraphText("Lost: " + userData.gameLost));
+// 		statFormDiv.appendChild(createParagraphText("Win Rate: " + userData.winRate + "%"));
+
+// 		const accountBoxDiv = createBoxDiv("accountBox");
+// 		accountBoxDiv.appendChild(createSubheadingText("Account"));
+// 		accountBoxDiv.appendChild(accountFormDiv);
+
+// 		const sessionBoxDiv = createBoxDiv("sessionBox");
+// 		sessionBoxDiv.appendChild(createSubheadingText("Session"));
+// 		sessionBoxDiv.appendChild(sessionFormDiv);
+
+// 		const statBoxDiv = createBoxDiv("statsBox");
+// 		statBoxDiv.appendChild(createSubheadingText("Game Stats"));
+// 		statBoxDiv.appendChild(statFormDiv);
+
+// 		appDiv.appendChild(createVideoBackgroundDiv("../backgrounds/Sauron.mp4"));
+// 		appDiv.appendChild(createLogoElement("../icons/sauron.png", "Barad-dûr Logo"));
+// 		appDiv.appendChild(createHeadingText("Lord of Transcendence"));
+// 		appDiv.appendChild(accountBoxDiv);
+// 		appDiv.appendChild(statBoxDiv);
+// 		appDiv.appendChild(sessionBoxDiv);
+// 	}
+// }
