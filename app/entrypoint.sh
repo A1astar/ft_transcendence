c26bf9ffb1f3011fe81d5cae3317fd84e0c5cb07
anska <103422661+algadea@users.noreply.github.com>
2025-11-28 03:07:46 +0100

    udpate to forward to other device

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+cd /frontend
 
+sed -i '/"extends": "..\/tsconfig.json",/d' tsconfig-local.json
 jq -s 'reduce .[] as $item ({}; . * $item)' "tsconfig-global.json" "tsconfig-local.json" > tsconfig.json
 jq -s 'reduce .[] as $item ({}; . * $item)' "package-global.json" "package-local.json" > package.json
 
 rm tsconfig-global.json tsconfig-local.json package-global.json package-local.json
 
-npm build
+# # Load NVM if it exists
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nginx -g daemon off
+# if ! command -v nvm &> /dev/null; then
+#     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+#     export NVM_DIR="$HOME/.nvm"
+#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# fi
+
+# if ! command -v node &> /dev/null || [ "$(node --version)" != "v24.11.0" ]; then
+#     nvm install 24.11.0
+#     nvm use 24.11.0
+# fi
+
+ls -l
+pwd
+npm install
+ls -l
+npm start
+
+nginx -g "daemon off;"
